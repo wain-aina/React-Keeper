3db155c1b4caca556d75a03c1dd313557d0da6bd
wain-aina <57414251+wain-aina@users.noreply.github.com>
2022-12-03 12:43:31 +0300

    Add files via upload

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Note from "./components/note";
+
+function App(){
+    return (
+        <div>
+            <Header/>
+            <Note/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
